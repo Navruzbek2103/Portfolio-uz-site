d87fac75a5d06d8a396386206d8d97a0c420fdaf
Eshonqul <ESHONQUL08012003AE>
2023-05-17 20:13:06 +0500

    change home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import "./style.scss";
-
-const index = () => {
-   return (
-      <div>
-         <h1>Home</h1>
-      </div>
-   );
-};
-
-export default index;
+export {default} from "./Home"
