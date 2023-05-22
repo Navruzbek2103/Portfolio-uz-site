1810941c859f1774c1e3b7195b567538a562912c
Navruzbek2103 <navrozbek2306@gmail.com>
2023-05-22 20:50:05 +0500

    Edit header

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Footer from "./components/Footer";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Blog from "./pages/Blog"
+import Contact from "./pages/Contact"
+import AllCards from "./pages/AllCards"
 
 
 const App = () => {
@@ -12,9 +15,11 @@ const App = () => {
       <Header />
 
         <Routes>
-        
             <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
+        <Route path='/allCards' element={<AllCards />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
         </Routes>
 
       <Footer />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,1 +1,175 @@
-export {default}  from "./Header"
+import React from 'react'
+import { NavLink } from "react-router-dom";
+import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa"
+import { RiInstagramFill, RiWhatsappFill } from "react-icons/ri"
+import { MdEmail } from "react-icons/md"
+import { TiLocation } from "react-icons/ti"
+import { IoIosHelpCircle } from "react-icons/io"
+import { FiSearch } from "react-icons/fi"
+import { HiUserCircle } from "react-icons/hi"
+import { FcLike } from "react-icons/fc"
+
+
+import Logo from "../../assets/images/Logo.svg"
+
+
+import "./style.scss"
+
+export default function Header() {
+
+
+
+  return (
+    <>
+      <header className='header'>
+
+        <div className='header__top'>
+          <div className='container'>
+            <div className='header__top-info'>
+              <ul className='d-flex align-items-center gap-3'>
+                <li className='header__top-link '>
+                  <a href=''>
+                    <FaFacebookF color='white' opacity='0.7' />
+                  </a>
+                </li>
+
+                <li className='header__top-link'>
+                  <a href=''>
+                    <FaTwitter color='white' opacity='0.7' />
+                  </a>
+                </li>
+
+                <li className='header__top-link'>
+                  <a href=''>
+                    <FaYoutube color='white' opacity='0.7' />
+                  </a>
+                </li>
+
+                <li className='header__top-link'>
+                  <a href=''>
+                    <RiInstagramFill color='white' opacity='0.7' />
+                  </a>
+                </li>
+
+                <li className='header__top-link'>
+                  <a href=''>
+                    <RiWhatsappFill color='white' opacity='0.7' />
+                  </a>
+                </li>
+
+                <li className='header__top-link'>
+                  <a href='tel: +971 55 811 9024'>
+                    +971 55 811 9024
+                  </a>
+                </li>
+
+                <li className='header__top-link'>
+                  <a href='mailto:request@example.com'>
+                    <MdEmail color='white' opacity='0.7' /> request@example.com
+                  </a>
+                </li>
+              </ul>
+
+              <div>
+                <div className='d-flex gap-3 align-items-center'>
+                  <div className='d-flex align-items-center'>
+                    <TiLocation size='13px' color='white' opacity='0.7' />
+                    <strong style={{ fontSize: "18px" }}
+                      className='text-white opacity-75  font-monospace'>Ship to:</strong>
+                  </div>
+
+                  <div>
+                    <select className='form-select' name="lang" id="">
+                      <option value="Canada">Canada</option>
+                      <option value="Use">Use</option>
+                      <option value="Rus">Rus</option>
+                    </select>
+                  </div>
+
+                  <div className='d-flex align-items-center gap-3'>
+                    <strong className='text-white opacity-75  font-monospace'>Currency:</strong>
+                    <select className='form-select' name="lang" id="">
+                      <option value="USE">USE</option>
+                      <option value="RUS">RUS</option>
+                      <option value="UZB">UZB</option>
+                    </select>
+                  </div>
+
+                  <a href=''
+                    className='text-white d-flex align-items-center gap-2 opacity-75 font-monospace'>
+                    <IoIosHelpCircle size='13px' color='white' opacity='0.7' />Help
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className='header__bottom'>
+          <div className="container">
+            <div className='d-flex align-items-center justify-content-between'>
+
+              <div className='d-flex align-items-center gap-5'>
+                <a href="">
+                  <img src={Logo} alt={Logo} />
+                </a>
+
+
+                <nav className=''>
+                  <ul>
+                    <li>
+                      <NavLink
+                        className={({ isActive }) => isActive ? "nav__link active" : "nav__link"}
+                        to="/allCards">Barcha portfoliolar</NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={({ isActive }) => isActive ? "nav__link active" : "nav__link"}
+                        to="/about" >Biz haqimizda</NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={({ isActive }) => isActive ? "nav__link active" : "nav__link"}
+                        to="/blog">Blog</NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={({ isActive }) => isActive ? "nav__link active" : "nav__link"}
+                        to="/contact">Bog'lanish</NavLink>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+
+
+              <div className='d-flex align-items-center gap-4'>
+                <ul className='d-flex gap-4'>
+                  <li>
+                    <button className='search-btn'>
+                      <FiSearch size='20px' />
+                    </button>
+                  </li>
+
+                  <li>
+                    <FcLike size='25px' />
+                  </li>
+
+                  <li>
+                    <HiUserCircle size='25px' />
+                  </li>
+                </ul>
+
+                <button className='btn btn-success'>Get a quote</button>
+              </div>
+
+
+            </div>
+          </div>
+
+
+        </div>
+      </header>
+    </>
+  )
+}
