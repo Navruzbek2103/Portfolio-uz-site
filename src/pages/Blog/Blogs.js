4693861522d6ac8cdb095895f3eac0f803b044b7
Navruzbek2103 <navrozbek2306@gmail.com>
2023-05-23 03:22:07 +0500

    Maked blog page

---
+++ src/pages/Blog/Blogs.js
@@ -0,0 +1,361 @@
+import BlogPageImageCard1 from "./../../assets/images/blog-page/blog-section-card-image-1.png"
+import BlogPageImageCard2 from "./../../assets/images/blog-page/blog-section-card-image-2.png"
+import BlogPageImageCard3 from "./../../assets/images/blog-page/blog-section-card-image-3.png"
+import BlogPageImageCard4 from "./../../assets/images/blog-page/blog-section-card-image-4.png"
+import BlogPageImageCard5 from "./../../assets/images/blog-page/blog-section-card-image-5.png"
+import BlogPageImageCard6 from "./../../assets/images/blog-page/blog-section-card-image-6.png"
+import BlogPageImageCard7 from "./../../assets/images/blog-page/blog-section-card-image-7.png"
+import BlogPageImageCard8 from "./../../assets/images/blog-page/blog-section-card-image-8.png"
+import BlogPageImageCard9 from "./../../assets/images/blog-page/blog-section-card-image-9.png"
+import BlogPageImageCard10 from "./../../assets/images/blog-page/blog-section-card-image-10.png"
+import BlogPageImageCard11 from "./../../assets/images/blog-page/blog-section-card-image-11.png"
+import BlogPageImageCard12 from "./../../assets/images/blog-page/blog-section-card-image-12.png"
+import BlogPageImageCard13 from "./../../assets/images/blog-page/blog-section-card-image-13.png"
+import BlogPageImageCard14 from "./../../assets/images/blog-page/blog-section-card-image-14.png"
+import BlogPageImageCard15 from "./../../assets/images/blog-page/blog-section-card-image-15.png"
+import BlogPageImageCard16 from "./../../assets/images/blog-page/blog-section-card-image-16.png"
+
+
+const allBlogs = [
+  {
+    cardId: 1,
+    cardTitle: "These new EV pick up trucks are about to be all the rage",
+    cardImg: BlogPageImageCard1,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+
+  {
+    cardId: 2,
+    cardTitle: "7 tips when buying a new car in UAE",
+    cardImg: BlogPageImageCard2,
+    cardType: "Guides",
+    cardDesc: "Platea dictumst quisque sagittis purus sit amet volutpat consequat. Porta nibh venenatis cras sed felis eget.",
+  },
+  {
+    cardId: 3,
+    cardTitle: "The rising cost of fuel in the UAE and how to avoid it",
+    cardImg: BlogPageImageCard3,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 4,
+    cardTitle: "Kia is operating eco-friendly vehicles at the World Cup",
+    cardImg: BlogPageImageCard4,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 5,
+    cardTitle: "The UAE’s Big Push Into Electric Vehicles Continues",
+    cardImg: BlogPageImageCard5,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 6,
+    cardTitle: "How to take the best shots of your car to get a sale",
+    cardImg: BlogPageImageCard6,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 7,
+    cardTitle: "Michelin launches construction on its first tire recycling plant in the world",
+    cardImg: BlogPageImageCard7,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 8,
+    cardTitle: "2021 Lexus LC500; a daily sports car",
+    cardImg: BlogPageImageCard8,
+    cardType: "Reviews",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 9,
+    cardTitle: "How to test drive a used car in UAE",
+    cardImg: BlogPageImageCard9,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 10,
+    cardTitle: "2023 Nissan Altima 2.0 SR Review: A Sensible, Somewhat Sporty Sedan",
+    cardImg: BlogPageImageCard10,
+    cardType: "Reviews",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 11,
+    cardTitle: "Will the flying car from GITEX become a reality?",
+    cardImg: BlogPageImageCard11,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 12,
+    cardTitle: "How to take the best shots of your car to get a sale",
+    cardImg: BlogPageImageCard12,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 13,
+    cardTitle: "GCC Specs a Comparison with Imported Specs Cars",
+    cardImg: BlogPageImageCard13,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 14,
+    cardTitle: "2021 Lexus LC500; a daily sports car",
+    cardImg: BlogPageImageCard14,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 15,
+    cardTitle: "What is a VIN (Vehicle Identification Number)?",
+    cardImg: BlogPageImageCard15,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 16,
+    cardTitle: "2021 Volvo S60; an underrated executive sedan",
+    cardImg: BlogPageImageCard16,
+    cardType: "Reviews",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 17,
+    cardTitle: "These new EV pick up trucks are about to be all the rage",
+    cardImg: BlogPageImageCard1,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+
+  {
+    cardId: 18,
+    cardTitle: "7 tips when buying a new car in UAE",
+    cardImg: BlogPageImageCard2,
+    cardType: "Guides",
+    cardDesc: "Platea dictumst quisque sagittis purus sit amet volutpat consequat. Porta nibh venenatis cras sed felis eget.",
+  },
+  {
+    cardId: 19,
+    cardTitle: "The rising cost of fuel in the UAE and how to avoid it",
+    cardImg: BlogPageImageCard3,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 20,
+    cardTitle: "Kia is operating eco-friendly vehicles at the World Cup",
+    cardImg: BlogPageImageCard4,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 21,
+    cardTitle: "The UAE’s Big Push Into Electric Vehicles Continues",
+    cardImg: BlogPageImageCard5,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 22,
+    cardTitle: "How to take the best shots of your car to get a sale",
+    cardImg: BlogPageImageCard6,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 23,
+    cardTitle: "Michelin launches construction on its first tire recycling plant in the world",
+    cardImg: BlogPageImageCard7,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 24,
+    cardTitle: "2021 Lexus LC500; a daily sports car",
+    cardImg: BlogPageImageCard8,
+    cardType: "Reviews",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 25,
+    cardTitle: "How to test drive a used car in UAE",
+    cardImg: BlogPageImageCard9,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 26,
+    cardTitle: "2023 Nissan Altima 2.0 SR Review: A Sensible, Somewhat Sporty Sedan",
+    cardImg: BlogPageImageCard10,
+    cardType: "Reviews",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 27,
+    cardTitle: "What is a VIN (Vehicle Identification Number)?",
+    cardImg: BlogPageImageCard11,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 28,
+    cardTitle: "How to take the best shots of your car to get a sale",
+    cardImg: BlogPageImageCard12,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 29,
+    cardTitle: "Michelin launches construction on its first tire recycling plant in the world",
+    cardImg: BlogPageImageCard13,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 30,
+    cardTitle: "2021 Lexus LC500; a daily sports car",
+    cardImg: BlogPageImageCard14,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 31,
+    cardTitle: "What is a VIN (Vehicle Identification Number)?",
+    cardImg: BlogPageImageCard15,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 32,
+    cardTitle: "Kia is operating eco-friendly vehicles at the World Cup",
+    cardImg: BlogPageImageCard16,
+    cardType: "Reviews",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 33,
+    cardTitle: "These new EV pick up trucks are about to be all the rage",
+    cardImg: BlogPageImageCard1,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+
+  {
+    cardId: 34,
+    cardTitle: "7 tips when buying a new car in UAE",
+    cardImg: BlogPageImageCard2,
+    cardType: "Guides",
+    cardDesc: "Platea dictumst quisque sagittis purus sit amet volutpat consequat. Porta nibh venenatis cras sed felis eget.",
+  },
+  {
+    cardId: 35,
+    cardTitle: "The rising cost of fuel in the UAE and how to avoid it",
+    cardImg: BlogPageImageCard3,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 36,
+    cardTitle: "Kia is operating eco-friendly vehicles at the World Cup",
+    cardImg: BlogPageImageCard4,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 37,
+    cardTitle: "The UAE’s Big Push Into Electric Vehicles Continues",
+    cardImg: BlogPageImageCard5,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 38,
+    cardTitle: "How to take the best shots of your car to get a sale",
+    cardImg: BlogPageImageCard6,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 39,
+    cardTitle: "Michelin launches construction on its first tire recycling plant in the world",
+    cardImg: BlogPageImageCard7,
+    cardType: "News",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 40,
+    cardTitle: "2021 Lexus LC500; a daily sports car",
+    cardImg: BlogPageImageCard8,
+    cardType: "Reviews",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 41,
+    cardTitle: "How to test drive a used car in UAE",
+    cardImg: BlogPageImageCard9,
+    cardType: "Guides",
+    cardDesc: "Accumsan sit amet nulla facilisi morbi tempus iaculis. Nisl nunc mi ipsum faucibus. Pulvinar neque laoreet suspendisse interdum consectetur.",
+  },
+  {
+    cardId: 42,
+    cardTitle: "2023 Nissan Altima 2.0 SR Review: A Sensible, Somewhat Sporty Sedan",
+    cardImg: BlogPageImageCard10,
+    cardType: "Reviews",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 43,
+    cardTitle: "What is a VIN (Vehicle Identification Number)?",
+    cardImg: BlogPageImageCard11,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 44,
+    cardTitle: "How to take the best shots of your car to get a sale",
+    cardImg: BlogPageImageCard12,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 45,
+    cardTitle: "Michelin launches construction on its first tire recycling plant in the world",
+    cardImg: BlogPageImageCard13,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 46,
+    cardTitle: "2021 Lexus LC500; a daily sports car",
+    cardImg: BlogPageImageCard14,
+    cardType: "Guides",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 47,
+    cardTitle: "What is a VIN (Vehicle Identification Number)?",
+    cardImg: BlogPageImageCard15,
+    cardType: "News",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    cardId: 481,
+    cardTitle: "Kia is operating eco-friendly vehicles at the World Cup",
+    cardImg: BlogPageImageCard16,
+    cardType: "Reviews",
+    cardDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+
+]
+export default allBlogs;
--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,9 +1,98 @@
 import React from 'react'
+import "./style.scss"
+import allBlogs from './Blogs.js'
+import { Link } from 'react-router-dom'
+import cardImage from "./../../assets/images/blog-page/blog-section-card-image-10.png"
 
 const index = () => {
+
+  let slicedBlogs1 = allBlogs.slice(0, 9)
+  let slicedBlogs2 = allBlogs.slice(9, 16)
+
+  const statusGuides = {
+    backgroundColor: "#E6F4FF",
+    color: "#0394FC",
+  }
+
+  const statusNews = {
+    backgroundColor: "#FFF1E8",
+    color: "#DD7437",
+  }
+
+  const statusReviews = {
+    backgroundColor: "#EEEAFE",
+    color: "#603FE2",
+  }
+
+
+
   return (
-    <div>Blog</div>
+    <div className='blog-page'>
+      <div className="container">
+        <div className="blog-page__top">
+          <h2 className="blog-page__top-title">Blog & News</h2>
+          <p className="blog-page__top-text">Company and car market news, as well as car selection guides and car reviews from our authors.</p>
+        </div>
+        <div className="blog-page__wrapper">
+          <div className="blog-page__wrapper-left">
+            <ul className="blog-page__wrapper-left-topList">
+              <li className="blog-page__wrapper-left-topItem">Categories:</li>
+              <li className="blog-page__wrapper-left-topItem" title='All the news on this site'>News</li>
+              <li className="blog-page__wrapper-left-topItem" title='All guides on this site'>Guides</li>
+              <li className="blog-page__wrapper-left-topItem" title='All reviews on this site'>Reviews</li>
+            </ul>
+            <ul className="blog-page__wrapper-left-cardList">
+              {
+                slicedBlogs1.map(({ cardId, cardImg, cardType, cardTitle, cardDesc }) => {
+
+                  return (
+                    <li className="blog-page__wrapper-left-cardItem" key={cardId}>
+                      <Link to={cardTitle.split(" ").join("-")} className='blog-page__wrapper-left-cardItem-link'>
+                        <div className="blog-page__wrapper-left-cardItem-link-imgBox">
+                          <img src={cardImg} alt="card image" className="blog-page__wrapper-left-cardItem-link-imgBox-img" />
+                        </div>
+                        <div className="blog-page__wrapper-left-cardItem-link-content">
+                          <p className="blog-page__wrapper-left-cardItem-link-content-status" style={cardType === "Guides" ? statusGuides : cardType === "News" ? statusNews : cardType === "Reviews" ? statusReviews : null}>{cardType}</p>
+                          <h3 className="blog-page__wrapper-left-cardItem-link-content-title">{cardTitle}</h3>
+                          <p className="blog-page__wrapper-left-cardItem-link-content-text">{cardDesc}</p>
+                        </div>
+                      </Link>
+                    </li>
+                  )
+                })
+              }
+
+            </ul>
+            <div className="blog-page__wrapper-left-pagination">Pagination is place</div>
+          </div>
+          <div className="blog-page__wrapper-right">
+            <h3 className="blog-page__wrapper-right-title">Featured posts</h3>
+            <ul className="blog-page__wrapper-right-list">
+              {
+                slicedBlogs2.map(({ cardId, cardImg, cardType, cardTitle }) => {
+                  return (
+                    <li className="blog-page__wrapper-right-item" key={cardId}>
+                      <Link to={cardTitle.split(" ").join("-")} className='blog-page__wrapper-right-item-link'>
+                        <div className="blog-page__wrapper-right-item-link-imgBox">
+                          <img src={cardImg} alt="card image" className="blog-page__wrapper-right-item-link-imgBox-img" />
+                        </div>
+                        <div className="blog-page__wrapper-right-item-link-content">
+                          <p className="blog-page__wrapper-right-item-link-content-status" style={cardType === "Guides" ? statusGuides : cardType === "News" ? statusNews : cardType === "Reviews" ? statusReviews : null}>{cardType}</p>
+                          <h4 className="blog-page__wrapper-right-item-link-content-title">{cardTitle}</h4>
+                        </div>
+                      </Link>
+                    </li>
+                  )
+                })
+              }
+
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default index
+export default index;
